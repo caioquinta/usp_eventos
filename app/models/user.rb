fbae8d5af01b1172493996523729d949dcb5ce03
Caio Quinta <caiodaquinta@gmail.com>
2016-04-07 17:00:35 -0300

    remove rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # events a user participates
   has_many :events
   has_many :events, through: :participants
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 
 Rails.application.routes.draw do
-  root :to => "users#new"
+  authenticated :user do
+    root 'users#home', as: :authenticated_root
+  end
+
+  root to: "users#index"
+  
   devise_for :users
+
+  resources :events
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
