14548596ba8327fec6cf9505e662d1420bac005c
Caio Quinta <caiodaquinta@gmail.com>
2016-04-19 20:59:12 -0300

    add user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create(name: 'USP EVENTOS', email: 'caiodaquinta@gmail.com', password: 'uspeventoscaio')
