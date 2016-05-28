d54c5b7a912509385a48213db1580aa48ac27802
Caio Quinta <caiodaquinta@gmail.com>
2016-05-28 12:51:46 -0300

    testing facebook on production

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env['omniauth.auth'])
+    puts request.env['omniauth.auth'].inspect
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
