f0db22bbc0916c3695d9ec877274527022afdf7b
Caio Quinta <caiodaquinta@gmail.com>
2016-05-28 12:05:15 -0300

    remove validates for user and email on suggestion

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,5 +1,3 @@
 class Suggestion < ActiveRecord::Base
-  validates :user_name, presence: true
   validates :description, presence: true
-  validates :email, presence: true
 end
--- spec/models/suggestion_spec.rb
+++ spec/models/suggestion_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe Suggestion, type: :model do
   let(:suggestion) { create :suggestion }
   context 'on creation' do
-    it 'must have a user_name' do
+    it 'should have a user_name' do
       expect(suggestion.user_name).to eql 'user'
     end
 
-    it 'dont create without a user_name' do
-      expect(build(:suggestion, user_name: '')).to_not be_valid
+    it 'create without a user_name' do
+      expect(build(:suggestion, user_name: '')).to be_valid
     end
 
     it 'must have a description' do
@@ -19,8 +19,8 @@ RSpec.describe Suggestion, type: :model do
       expect(build(:suggestion, description: '')).to_not be_valid
     end
 
-    it 'dont create without an email' do
-      expect(build(:suggestion, email: '')).to_not be_valid
+    it 'create without an email' do
+      expect(build(:suggestion, email: '')).to be_valid
     end
   end
 end
