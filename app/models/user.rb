9d010e7d6aff848c3fe12e092f0c47444ae0bdb2
Caio Quinta <caiodaquinta@gmail.com>
2016-06-02 11:08:24 -0300

    not necessary anymore

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,10 @@ class User < ActiveRecord::Base
   devise :omniauthable, omniauth_providers: [:facebook]
 
   def self.from_omniauth(auth)
-    puts 'DEBUG AUTH: ' + auth.inspect
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
-      puts 'DEBUG USER: ' + user.inspect
     end
   end
 end
