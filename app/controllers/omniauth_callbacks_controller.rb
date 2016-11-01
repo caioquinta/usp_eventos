de5259d05f67dec8f8a226ba5c33b32ed3217a28
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2016-11-01 19:17:02 -0200

    include omniauth-google

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -9,6 +9,16 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
+  def google_oauth2
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'google_oauth2') if is_navigational_format?
+    else
+      session['devise.google_oauth2_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
 
   def failure
     redirect_to root_path
